67435152c20696289650761b9efa741de942f691
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-05-19 16:20:56 +0530

    data ingestion step completed

---
+++ visa/utils/main_utils.py
@@ -0,0 +1,16 @@
+import os, sys
+import yaml 
+import numpy as np
+import dill
+import pandas as pd
+from visa.constant import *
+from visa.exception import CustomException
+from visa.logger import logging
+
+
+def read_yaml_file(file_path:str) -> dict:
+    try:
+        with open(file_path, 'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise CustomException(e, sys) from e
